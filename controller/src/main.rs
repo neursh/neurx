e687dff26ac924e94aeb4d89e4963cc12814e284
neursh <neurs@spaark.click>
2025-01-07 15:58:28 +0700

    Controller for keyboard

---
+++ controller/src/main.rs
@@ -0,0 +1,71 @@
+#![no_std]
+#![no_main]
+
+use core::sync::atomic::{AtomicBool, Ordering};
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_rp::{bind_interrupts, peripherals::USB, usb::{Driver, InterruptHandler}};
+use embassy_usb::{class::hid::State, Builder, Config, Handler};
+
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => InterruptHandler<USB>;
+});
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let peripherals = embassy_rp::init(Default::default());
+    let driver = Driver::new(peripherals.USB, Irqs);
+
+    let mut config = Config::new(14521, 1819);
+    config.manufacturer = Some("Neurs");
+    config.product = Some("Neurx");
+    config.serial_number = Some("12345678");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut msos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+    let mut request_handler = RequestHandler {};
+    let mut device_handler = DeviceHandler::new();
+
+    let mut state = State::new();
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut msos_descriptor,
+        &mut control_buf,
+    );
+
+    builder.handler(&mut device_handler);
+}
+
+struct RequestHandler {}
+
+struct DeviceHandler {
+    configured: AtomicBool,
+}
+
+impl DeviceHandler {
+    fn new() -> Self {
+        DeviceHandler {
+            configured: AtomicBool::new(false),
+        }
+    }
+}
+
+impl Handler for DeviceHandler {
+    fn enabled(&mut self, enabled: bool) {
+        self.configured.store(false, Ordering::Relaxed);
+        if enabled {
+            info!("Device enabled");
+        } else {
+            info!("Device disabled");
+        }
+    }
+}
